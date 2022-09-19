a1eae1f115c54ed7384b78bc036ed25315a5c0d0
iSagArSutAr <113018662+iSagArSutAr@users.noreply.github.com>
2022-09-19 08:52:16 +0530

    Add files via upload

---
+++ Day7/src/StringDemo.java
@@ -0,0 +1,14 @@
+
+public class StringDemo {
+	public static void main(String[] args) {
+		String s1=new String("java");
+		String s2="java";
+		System.out.println(s1);
+		System.out.println(s2);
+		if(s1==s2)
+			System.out.println("Equal");
+		else System.out.println("Not equal");
+		
+		System.out.println();
+	}
+}
